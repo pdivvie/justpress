320c0760ef424e9fce41498c7e6996291e24a6f8
pdivvie <pdivvie@outlook.com>
2019-08-11 03:41:44 +0200

    Implemented bootstrap as well as templates for the body, nav and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
 module ApplicationHelper
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: root_path,
+        title: 'Games'
+      },
+      {
+        url: root_path,
+        title: 'Match Database'
+      },
+      {
+        url: root_path,
+        title: 'Local Players'
+      },
+      {
+        url: root_path,
+        title: 'Articles'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
