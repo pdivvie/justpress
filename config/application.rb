0efda9499c1f9e77ca71f811012e05611b8d665b
pdivvie <pdivvie@outlook.com>
2019-08-16 12:27:07 +0200

    Declared ruby version and configured eager load paths

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module SafgcApp
 
     config.assets.initialize_on_precompile = false
 
+    config.eager_load_paths << "#{Rails.root}/lib"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
