bc2dfe71f764ccdcca053899e045b00d41afe642
pdivvie <pdivvie@outlook.com>
2019-10-01 12:36:46 +0200

    Ran generator for controller and model for characters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_131130) do
+ActiveRecord::Schema.define(version: 2019_10_01_103437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2019_09_30_131130) do
     t.datetime "updated_at", null: false
     t.bigint "topic_id"
     t.index ["topic_id"], name: "index_articles_on_topic_id"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "title"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "games", force: :cascade do |t|
