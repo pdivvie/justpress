14889fdcda4abb554a10f28680bfa4ff8a16b3b7
pdivvie <pdivvie@localhost.localdomain>
2021-01-22 11:51:33 +0200

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "pdivvie@outlook.com",
+  username: "admin",
+  password: "password",
+  password_confirmation: "password"
+)
 
-3.times do |topic|
+puts "1 user created with username 'admin' and password 'password'\nRun 'User.find_by(username: 'admin').add_role :admin' \nin rails console to make user an admin"
+
+User.create!(
+  email: "test@test.com",
+  username: "test",
+  password: "password",
+  password_confirmation: "password"
+)
+
+puts "1 test user created with username 'test' and password 'password'"
+
+2.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
     )
 end
+
+puts "2 topics created"
+
+3.times do |game|
+  Game.create!(
+    title: "Game #{game}",
+    description: "Lorem Ipsum etc etc Lorem Ipsum etc etc Lorem Ipsum etc etc Lorem Ipsum etc etc",
+    cover_image: "https://via.placeholder.com/150"
+		)
+end
+
+puts "3 games created"
